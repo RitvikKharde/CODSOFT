b6285ea828776f1cb7cc84a7ef22c595ea34c782
RitvikKharde <kharderitvik9@gmail.com>
2024-06-22 20:43:20 +0530

    Add files via upload
    TASK 5
    CODSOFT INTERNSHIP FOR PYTHON PROGRAMMING

---
+++ contactbook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#CONTACT_BOOK
+names = []
+contact_numbers = []
+num = int(input("Enter the total number of contacts you want to save: "))
+for i in range(num):
+    name = input("Name: ")
+    contact_number = int(input("Contact Number: ")) 
+    names.append(name)
+    contact_numbers.append(contact_number)
+print("\nName\t\t\tContact Number\n")
+for i in range(num):
+    print("{}\t\t\t{}".format(names[i], contact_numbers[i]))
+search_term = input("\nEnter search Name: ")
+print("Search result:")
+if search_term in names:
+    index = names.index(search_term)
+    contact_number = contact_numbers[index]
+    print("Name: {}, Phone Number: {}".format(search_term, contact_number))
+else:
+    print("No records")
+
