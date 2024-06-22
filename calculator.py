65d61d9d69b965437f308b30e54de6d540f4d420
RitvikKharde <kharderitvik9@gmail.com>
2024-06-22 20:39:58 +0530

    Add files via upload
    TASK 2 
    CODSOFT INTERNSHIP FOR PYTHON PROGRAMMING

---
+++ calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#CALCULATOR
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
+
